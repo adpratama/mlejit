57b824a36ce3525887f610445438706e0c56d18a
adpratama <adpratama.adp@gmail.com>
2025-01-02 15:04:17 +0700

    update: tambah akses mbak elis

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -105,6 +105,39 @@
                         <span>Customer</span>
                     </a>
                 </li>
+                <li class="nav-heading">Invoice Mlejit Coffee</li>
+                <li class="nav-item">
+                    <a class="nav-link <?php if ($this->uri->segment(2) != 'invoice') echo 'collapsed' ?>" href="<?= base_url() ?>admin/invoice">
+                        <i class="bi bi-journal-check"></i>
+                        <span>Invoice</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link <?php if ($this->uri->segment(2) != 'customer') echo 'collapsed' ?>" href="<?= base_url() ?>admin/customer">
+                        <i class="bi bi-journal-check"></i>
+                        <span>Customer</span>
+                    </a>
+                </li>
+                <li class="nav-heading">Transaction</li>
+                <li class="nav-item">
+                    <a class="nav-link <?php if ($this->uri->segment(2) != 'transaction') echo 'collapsed' ?>" href="<?= base_url() ?>admin/transaction">
+                        <i class="bi bi-journal-check"></i>
+                        <span>Transaction</span>
+                    </a>
+                </li>
+                <li class="nav-heading">Products</li>
+                <li class="nav-item">
+                    <a class="nav-link <?php if ($this->uri->segment(2) != 'product') echo 'collapsed' ?>" href="<?= base_url() ?>admin/product">
+                        <i class="bi bi-archive"></i>
+                        <span>Products</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link <?php if ($this->uri->segment(2) != 'category') echo 'collapsed' ?>" href="<?= base_url() ?>admin/category">
+                        <i class="bi bi-list-check"></i>
+                        <span>Categories</span>
+                    </a>
+                </li>
             <?php
             } else {
             ?>
